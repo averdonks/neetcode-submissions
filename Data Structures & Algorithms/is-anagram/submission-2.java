b2676b69b45fe3203b1162c125713612a84c566e
Anthony V <62974106+averdonks@users.noreply.github.com>
2026-06-26 12:34:16 -0400

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+
+        if (Arrays.equals(sChars, tChars))
+            return true;
+        return false;
+    }
+}
