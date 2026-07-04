018acdef510e6fce3a4fe8fb68823c785ee3f08c
Anthony V <62974106+averdonks@users.noreply.github.com>
2026-07-04 15:55:56 -0400

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.java
@@ -0,0 +1,17 @@
+// Review
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+
+        Map<Character, Integer> mapS = new HashMap<>();
+        Map<Character, Integer> mapT = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            mapS.put(s.charAt(i), mapS.getOrDefault(s.charAt(i), 0) + 1); // Checked previous solution
+            mapT.put(t.charAt(i), mapT.getOrDefault(t.charAt(i), 0) + 1);
+        }
+
+        return mapS.equals(mapT);
+    }
+}
